f0ea404d9b3b08107815e7000a32dfd007c4d2d0
Tyler Hebert <tyler.hebert@hotmail.com>
2020-02-24 20:57:07 -0600

    Modified filepath
    Corrected file path to not include eclipse-workspace

--- eclipse-workspace/SEDatabaseProject/SEDatabaseProject/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.openjfx.SEDatabaseProject {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires javafx.base;
-	requires javafx.graphics;
-
-    opens org.openjfx.SEDatabaseProject to javafx.fxml;
-    exports org.openjfx.SEDatabaseProject;
-}
